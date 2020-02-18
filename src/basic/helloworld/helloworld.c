2099aaab64cd6cb675508229bb1b1c84467d243a
devlights <gsf.zero1@devlights.onmicrosoft.com>
2020-02-18 13:37:52 +0900

    try-golang のようにサンプル名を受け付けて実行する形式に調整

--- src/basic/helloworld/helloworld.c
+++ src/basic/helloworld/helloworld.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 
-void basic_hello_world01() { 
+void basic_helloworld01() {
     printf("Hello World\n"); 
 }
--- src/commonfuncs.c
+++ src/commonfuncs.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include "examples.h"
 
-void do_it(void (*func)(), char *message) {
-    printf("[START] ------- %s -------\n", message);
-    func();
-    printf("[ END ] ------- %s -------\n", message);
+int example_count = 0;
+example examples[MAX_EXAMPLE_COUNT];
+
+void make_mappings() {
+
+    for (int i = 0; i < MAX_EXAMPLE_COUNT; i++) {
+        examples[i] = (example) {"end", NULL};
+    }
+
+    /*
+     * basic
+     */
+    int index = 0;
+    examples[index++] = (example) {"basic_helloworld", basic_helloworld01};
+    examples[index++] = (example) {"basic_chararray_01", basic_char_array01};
+    examples[index++] = (example) {"basic_chararray_02", basic_char_array02};
+    examples[index++] = (example) {"basic_chararray_03", basic_char_array03};
+    examples[index++] = (example) {"basic_chararray_04", basic_char_array04};
+    examples[index++] = (example) {"basic_forloop_01", basic_for_loop01};
+    examples[index++] = (example) {"basic_detect_c_version", basic_detect_c_lang_version};
+
+    example_count = index;
 }
