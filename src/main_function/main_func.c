10798df58acd2906b9e09057af6d402be5a75722
devlights <gsf.zero1@devlights.onmicrosoft.com>
2020-06-29 20:36:48 +0900

    Add main function example

---
+++ src/main_function/main_func.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// main_function -- main 関数のサンプルです.
+//
+// REFERENCES:
+//   - https://qiita.com/raccy/items/b59bd7fa9f4eacabca79
+//   - http://www.open-std.org/jtc1/sc22/wg14/www/docs/n1570.pdf
+int main(void) {
+    // C言語 において 現状の最新仕様 C11 で有効な main 関数の書式は
+    //
+    // - int main(void)
+    // - int main(int argc, char *argv[])
+    // - int main(int args, char **argv)
+    //
+    // のどれかとならなけばいけない。
+    printf("有効な main 関数は\n");
+    printf("\tint main(void)\n");
+    printf("\tint main(int argc, char *argv[])\n");
+    printf("\tint main(int argc, char **argv)\n");
+    printf("のどれかで無いといけない");
+
+    return 0;
+}
+
