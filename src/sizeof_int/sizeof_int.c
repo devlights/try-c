fdcf588af5f033fef5e203290aea530afacb144a
devlights <gsf.zero1@devlights.onmicrosoft.com>
2020-07-01 01:33:36 +0000

    Add sizeof_int example

---
+++ src/sizeof_int/sizeof_int.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    // sizeof() は、size_t を返す
+    size_t intSize = sizeof(int);
+
+    // size_t は unsigned long なので %lu を指定
+    printf("sizeof(int): %lu bytes, %lu bits\n", intSize, intSize*8);
+
+    return 0;
+}
